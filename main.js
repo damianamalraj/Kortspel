be9cf4c489a880d53e7b853802971b9512bd39cb
SJKed <simonjurstedtskolan@gmail.com>
2021-10-21 11:55:17 +0200

    jeff

---
+++ main.js
@@ -0,0 +1,71 @@
+let CHearts = [{
+        suit: "Heart",
+        number: "1"
+    },
+    {
+        suit: "Heart",
+        number: "2"
+    },
+    {
+        suit: "Heart",
+        number: "3"
+    },
+    {
+        suit: "Heart",
+        number: "4"
+    },
+    {
+        suit: "Heart",
+        number: "5"
+    },
+    {
+        suit: "Heart",
+        number: "6"
+    },
+    {
+        suit: "Heart",
+        number: "7"
+    },
+    {
+        suit: "Heart",
+        number: "8"
+    },
+    {
+        suit: "Heart",
+        number: "9"
+    },
+    {
+        suit: "Heart",
+        number: "10"
+    },
+    {
+        suit: "Heart",
+        number: "11"
+    },
+    {
+        suit: "Heart",
+        number: "12"
+    },
+    {
+        suit: "Heart",
+        number: "13"
+    }
+]
+let CSpades = [
+    {
+        suit: "Spade",
+        number: ""
+    }
+]
+
+if (currentCard == nextCard){
+
+}
+
+if (currentCard > nextCard) {
+
+}
+
+if (currentCard < nextCard) {
+
+}
