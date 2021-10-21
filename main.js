d0ba7e095c75c9bfdef55aa585f800382ba0c3b0
damianamalraj <damian.amalraj@hotmail.com>
2021-10-21 16:28:39 +0200

    1

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ let Cards = [];
 for (let i = 1; i < 13 + 1; i++) {
     Cards.push({
         suits: "&hearts;",
-        number: i
+        number: i,
     });
     Cards.push({
         suits: "&diams;",
-        number: i
+        number: i,
     });
     Cards.push({
         suits: "&spades;",
-        number: i
+        number: i,
     });
     Cards.push({
         suits: "&clubs;",
-        number: i
+        number: i,
     });
 }
 
@@ -24,12 +24,29 @@ const tries = document.querySelector(".tries");
 const value = document.querySelectorAll(".value");
 const suits = document.querySelectorAll(".suit");
 
+const cardsLeft = document.querySelector(".klar");
+
 const buttonLower = document.querySelector(".BLower");
 const buttonEqual = document.querySelector(".BEqual");
 const buttonHigher = document.querySelector(".BHigher");
 
-buttonLower.addEventListener("click", Event => {
-    let randomNum = Math.floor(Math.random() * Cards.length)
+let point = 0;
+let trie = 3;
+
+buttonLower.addEventListener("click", (Event) => {
+    let randomNum = Math.floor(Math.random() * Cards.length);
+
+    if (trie != 0) {
+        if (value.innerText > Cards[randomNum].number) {
+            point++;
+            points.innerHTML = `Poäng: ${point}`;
+        } else {
+            trie--;
+            tries.innerHTML = `Försök kvar: ${trie}`;
+        }
+    } else {
+    }
+
     for (i = 0; i < value.length; i++) {
         value[i].innerText = Cards[randomNum].number;
     }
@@ -37,10 +54,24 @@ buttonLower.addEventListener("click", Event => {
     for (i = 0; i < suits.length; i++) {
         suits[i].innerHTML = Cards[randomNum].suits;
     }
-})
+    Cards.splice(randomNum, 1);
+    cardsLeft.innerText = Cards.length;
+});
 
-buttonEqual.addEventListener("click", Event => {
-    let randomNum = Math.floor(Math.random() * Cards.length)
+buttonEqual.addEventListener("click", (Event) => {
+    let randomNum = Math.floor(Math.random() * Cards.length);
+
+    if (trie != 0) {
+        if (value.innerText == Cards[randomNum].number) {
+            point++;
+            points.innerHTML = `Poäng: ${point}`;
+        } else {
+            trie--;
+            tries.innerHTML = `Försök kvar: ${trie}`;
+        }
+    } else {
+    }
+
     for (i = 0; i < value.length; i++) {
         value[i].innerText = Cards[randomNum].number;
     }
@@ -48,10 +79,24 @@ buttonEqual.addEventListener("click", Event => {
     for (i = 0; i < suits.length; i++) {
         suits[i].innerHTML = Cards[randomNum].suits;
     }
-})
+    Cards.splice(randomNum, 1);
+    cardsLeft.innerText = Cards.length;
+});
 
-buttonHigher.addEventListener("click", Event => {
-    let randomNum = Math.floor(Math.random() * Cards.length)
+buttonHigher.addEventListener("click", (Event) => {
+    let randomNum = Math.floor(Math.random() * Cards.length);
+
+    if (trie != 0) {
+        if (value.innerText < Cards[randomNum].number) {
+            point++;
+            points.innerHTML = `Poäng: ${point}`;
+        } else {
+            trie--;
+            tries.innerHTML = `Försök kvar: ${trie}`;
+        }
+    } else {
+    }
+
     for (i = 0; i < value.length; i++) {
         value[i].innerText = Cards[randomNum].number;
     }
@@ -59,4 +104,6 @@ buttonHigher.addEventListener("click", Event => {
     for (i = 0; i < suits.length; i++) {
         suits[i].innerHTML = Cards[randomNum].suits;
     }
-})
+    Cards.splice(randomNum, 1);
+    cardsLeft.innerText = Cards.length;
+});
